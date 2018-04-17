ae91aeb0124626a72e9fe35cb5044535f1362a97
Jahmanz <savojah@yahoo.com>
2018-04-17 09:46:51 -0700

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,18 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import MyStyledComponent from './MyStyledComponent';
+import NewTicketForm from './NewTicketForm'
 import { Switch, Route } from 'react-router-dom';
+
 function App(){
   return (
     <div>
-      <style jsx global>{`
-        body {
-          font-family: Helvetica;
-        }
-        .box {
-          border: none;
-          display: block;
-          border-bottom: 2px solid #fff;
-          margin-bottom: 10px;
-        }
-        .box:hover {
-          border-bottom: 2px solid #ccc;
-          outline: 0;
-        }
-        .color-toggle {
-          background-color: green;
-        }
-        .color-toggle:hover {
-          background-color: pink;
-        }
-        a {
-          color: #888;
-          text-decoration: none;
-        }
-      `}</style>
-    <div style={{margin: '19px auto 0', width: 600}}>
-        <a href="" target="_blank">
-          <div className="box">
-              <Header className="box color-toggle"/>
-              <TicketList/>
-              <MyStyledComponent/>
-          </div>
-        </a>
-      </div>
-  </div>
-    // <div>
-    //   <Header/>
-    //   <TicketList/>
-    //   <MyStyledComponent/>
-    // </div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='newticket' component={NewTicketForm} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Header(){
   return (
-    <h1 className="box color-toggle">Help Queue</h1>
-  );
+    <h1>Help Queue</h1>
+    <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>  );
 }
 
 export default Header;
