716d290e3c56e1f69490efdc60397de014b98eaa
Alexander Neumann <alexander.daniel.neumann@gmail.com>
2018-04-23 08:25:56 -0700

    Add first callback function for solving Unidirectional dataflow.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
 class NewTicketControl extends React.Component {
 
   constructor(props) {
@@ -7,15 +8,26 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
   }
 
   render(){
-    return (
-      <div>
-        <p>This is the NewTicketControl component!</p>
-      </div>
-    );
-  }
+      let currentlyVisibleContent = null;
+      if (this.state.formVisibleOnPage){
+        currentlyVisibleContent = <NewTicketForm />;
+      } else {
+        currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>
+      }
+      return (
+        <div>
+          {currentlyVisibleContent}
+        </div>
+      );
+    }
 }
 
 export default NewTicketControl;
